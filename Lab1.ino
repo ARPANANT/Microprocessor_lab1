4373fd6fb05aa3f610bc78174382c00384b9cad7
ARPANANT1096 <60213683+ARPANANT@users.noreply.github.com>
2020-01-23 21:42:49 +0700

    Update Lab1.ino

--- Lab1.ino
+++ Lab1.ino
@@ -12,13 +12,13 @@ void loop()
   PORTB = 0x00;
   for(a=0;a<6;a++)
   {
-      delay(200);
+      delay(1000);
       PORTD=PORTD<<1;
   }
   PORTB = 0b000001;
   for(b=0;b<2;b++)
   {
-    delay(200);
+    delay(1000);
     PORTB = PORTB <<1;
   }
 }
@@ -47,13 +47,13 @@ void loop()
   PORTD = 0b00000100;
   for(a=0;a<6;a++)
   {
-    delay(200);
+    delay(1000);
     PORTD = PORTD << 1;
   }
   PORTB = 0b000001;
   for(b=0;b<2;b++)
   {
-    delay(200);
+    delay(1000);
     PORTB = PORTB << 1;
   } 
  } 
@@ -62,13 +62,13 @@ void loop()
   PORTB = 0b000010;
   for(b=0;b<2;b++)
   {
-    delay(200);
+    delay(1000);
     PORTB = PORTB >> 1;
   }    
   PORTD = 0b10000000;
   for(a=0;a<6;a++)
   {
-    delay(200);
+    delay(1000);
     PORTD = PORTD >> 1;
   }
  }
